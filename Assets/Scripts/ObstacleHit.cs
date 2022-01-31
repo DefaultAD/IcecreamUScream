c9f3f2e830fb256313c27c294d3edd799bf7cbd6
joshuavanbreda <joshuavanbreda@gmail.com>
2022-01-31 02:36:44 +0200

    new changes
    new changes

---
+++ Assets/Scripts/ObstacleHit.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHit : MonoBehaviour
+{
+    public Stack stack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "StackedScoop")
+        {
+            for (int i = 0; i < stack.stackObjects.Count; i++)
+            {
+                if (stack.stackObjects[i] == other.gameObject)
+                {
+                    stack.GetObjects(stack.stackObjects[i], i);
+                    //stack.stackObjects.Remove(stack.stackObjects[i]);
+                    //Destroy(stack.stackObjects[i]);
+                    other.gameObject.SetActive(false);
+                }
+            }
+            GetComponent<Collider>().enabled = false;
+        }
+    }
+}
