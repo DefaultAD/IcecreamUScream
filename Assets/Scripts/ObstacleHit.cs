7a037c9e941a870441c0c98abb114c02a6f1228b
DefaultAD <dewaldtdevilliers@gmail.com>
2022-02-01 02:18:41 +0200

    Revert "1"
    This reverts commit db7c016021c18062286d43e9c9738afb7701a39d.

--- Assets/Scripts/ObstacleHit.cs
+++ Assets/Scripts/ObstacleHit.cs
@@ -21,10 +21,9 @@ public class ObstacleHit : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        //GetComponent<Collider>().enabled = false;
+        GetComponent<Collider>().enabled = false;
         if (other.tag == "StackedScoop")
         {
-            GetComponent<Collider>().enabled = false;
             for (int i = 0; i < stack.stackObjects.Count; i++)
             {
                 if (stack.stackObjects[i] == other.gameObject)
@@ -36,23 +35,9 @@ public class ObstacleHit : MonoBehaviour
 
                     //currentScoopHit = stack.stackObjects[i];
 
-                    
-                    if (this.gameObject.tag == "Spoon")
-                    {
-                        newCaughtScoop = Instantiate(stack.stackObjects[i], this.gameObject.transform.position, stack.stackObjects[i].transform.rotation);
-                        newCaughtScoop.transform.parent = this.gameObject.transform;
-                        newCaughtScoop.transform.localPosition = new Vector3(0f, -0.02f, 0.175f);
-                    }
-                    if (this.gameObject.tag == "Tongue")
-                    {
-                        newCaughtScoop = Instantiate(stack.stackObjects[i], this.gameObject.transform.position, stack.stackObjects[i].transform.rotation);
-                        newCaughtScoop.transform.parent = this.gameObject.transform;
-                        newCaughtScoop.transform.localPosition = new Vector3(0f, 0.35f, 0f);
-                        foreach (var c in this.gameObject.GetComponentsInChildren<Collider>())
-                        {
-                            if (c.isTrigger) c.enabled = false;
-                        }
-                    }
+                    newCaughtScoop = Instantiate(stack.stackObjects[i], this.gameObject.transform.position, stack.stackObjects[i].transform.rotation);
+                    newCaughtScoop.transform.parent = this.gameObject.transform;
+                    newCaughtScoop.transform.localPosition = new Vector3(0f, -0.02f, 0.175f);
 
                     stack.stackObjects[i].gameObject.SetActive(false);
                     Debug.Log("loop count");
